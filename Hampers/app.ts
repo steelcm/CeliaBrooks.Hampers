7c57dceb91563539a7636704d3b7fb813bcc1662
Chris Steel <csteel@gmail.com>
2015-08-24 20:09:48 +0100

    Initial commit

---
+++ Hampers/app.ts
@@ -0,0 +1,66 @@
+﻿/// <reference path="typings/tsd.d.ts" />
+
+import express = require('express');
+import user = require('./routes/user');
+import http = require('http');
+import path = require('path');
+import errorHandler = require('errorhandler');
+import logger = require('morgan');
+import cookieParser = require('cookie-parser');
+import bodyParser = require('body-parser');
+
+import routes = require('./routes/index');
+import users = require('./routes/user');
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err['status'] = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+
+    app.use((err: any, req, res, next) => {
+        res.status(err['status'] || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err: any, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+//export = app;
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ Hampers/gulpfile.ts
@@ -0,0 +1,44 @@
+﻿import gulp = require('gulp');
+import sass = require('gulp-sass');
+import nodemon = require('gulp-nodemon');
+import browserSync = require('browser-sync');
+
+var watchFiles = ["app.js", "routes/*.js", "views/*.jade", "assets/sass/**/*.scss"];
+
+gulp.task('default', ['browser-sync', 'styles-watch'], () => { });
+
+gulp.task('browser-sync', ['nodemon'], () => {
+    console.log("executed");
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: watchFiles,
+        browser: "firefox",
+        port: 7000,
+        reloadDelay: 1000
+    });
+});
+
+gulp.task('nodemon', (cb) => {
+    var started = false;
+
+    return nodemon({
+        script: 'app'
+    }).on('start', function () {
+        // to avoid nodemon being started multiple times
+        // thanks @matthisk
+        if (!started) {
+            cb();
+            started = true;
+        }
+    });
+});
+
+gulp.task('styles', () => {
+    gulp.src('assets/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/stylesheets/'));
+});
+
+gulp.task('styles-watch', () => {
+    gulp.watch('assets/sass/**/*.scss', ['styles']);
+});
