cc5f0597af9e5f810448ce02c84e103c0fd74c0c
Chris Steel <csteel@gmail.com>
2015-09-03 14:55:28 +0100

    Typescript compile added to gulp compilation task.

--- Hampers/gulpfile.ts
+++ Hampers/gulpfile.ts
@@ -1,11 +1,14 @@
-﻿import gulp = require('gulp');
+﻿/// <reference path="typings/tsd.d.ts" />
+
+import gulp = require('gulp');
 import sass = require('gulp-sass');
 import nodemon = require('gulp-nodemon');
 import browserSync = require('browser-sync');
+import typeScript = require('gulp-typescript');
 
 var watchFiles = ["app.js", "routes/*.js", "views/*.jade", "assets/sass/**/*.scss"];
 
-gulp.task('default', ['browser-sync', 'styles-watch'], () => { });
+gulp.task('default', ['browser-sync', 'styles-watch', 'styles-build', 'js-watch', 'js-build'], () => { });
 
 gulp.task('browser-sync', ['nodemon'], () => {
     console.log("executed");
@@ -33,12 +36,23 @@ gulp.task('nodemon', (cb) => {
     });
 });
 
-gulp.task('styles', () => {
+gulp.task('styles-build', () => {
     gulp.src('assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('public/stylesheets/'));
 });
 
 gulp.task('styles-watch', () => {
-    gulp.watch('assets/sass/**/*.scss', ['styles']);
+    gulp.watch('assets/sass/**/*.scss', ['styles-build']);
+});
+
+
+var jsWatchFiles = ['**/*.ts', '!**/*.d.ts', '!node_modules/**/*.ts'];
+gulp.task('js-build', () => {
+    gulp.src(jsWatchFiles)
+        .pipe(typeScript());
+});
+
+gulp.task('js-watch', () => {
+    gulp.watch(jsWatchFiles, ['js-build'])
 });
--- Hampers/routes/index.ts
+++ Hampers/routes/index.ts
@@ -1,7 +1,9 @@
-﻿import express = require('express');
+﻿/// <reference path="../typings/tsd.d.ts" />
+
+import express = require('express');
 var router = express.Router();
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express is AWESOME' });
 });
 export = router;
